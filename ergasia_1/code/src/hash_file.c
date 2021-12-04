9e3c168e969a6ccf5bb68bb3fe34bf521db7de59
Alex-techlab-dotcom <chalassproduction@gmail.com>
2021-12-04 18:54:29 +0200

    do the insertion

--- ergasia_1/code/src/hash_file.c
+++ ergasia_1/code/src/hash_file.c
@@ -7,7 +7,6 @@
 #include "../examples/exhashtable.h"
 
 
-
 #define CALL_BF(call)       \
 {                           \
   BF_ErrorCode code = call; \
@@ -20,7 +19,7 @@
 HT_ErrorCode HT_Init() {
     //insert code here
     for ( int i = 0; i < MAX_OPEN_FILES; ++i ) {
-        OpenHashFiles[i].FileName=NULL;
+        OpenHashFiles[i].FileName = NULL;
     }
     return HT_OK;
 }
@@ -28,7 +27,7 @@ HT_ErrorCode HT_Init() {
 HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
 
     int blockFileID;
-   // CALL_BF(BF_Init(LRU));//it uses the LRU method to clear the unneeded blocks from the buffer
+    // CALL_BF(BF_Init(LRU));//it uses the LRU method to clear the unneeded blocks from the buffer
     CALL_BF(BF_CreateFile(filename))
     CALL_BF(BF_OpenFile(filename, &blockFileID));// it gives the data1.db file a specific ID(lets say ID=11)
 
@@ -41,26 +40,27 @@ HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
     CALL_BF(BF_AllocateBlock(blockFileID, firstBlock));
     CALL_BF(BF_AllocateBlock(blockFileID, secondBlock));
 
-    char * firstBlockData = BF_Block_GetData(firstBlock);
+    char *firstBlockData = BF_Block_GetData(firstBlock);
 
     memmove(firstBlockData, "HF", strlen("HF"));
-    memset(firstBlockData + strlen("HF")*sizeof(char), depth, sizeof(int));
+    memcpy(firstBlockData + strlen("HF") * sizeof(char), &depth, sizeof(int));
     BF_Block_SetDirty(firstBlock);
 
     //char* blockData;
-    char * secondBlockData = BF_Block_GetData(secondBlock);
-    int sz=(int)pow(2,depth);
+    char *secondBlockData = BF_Block_GetData(secondBlock);
+    int sz = (int) pow(2, depth);
 
-    for ( int i = 0; i <sz ; ++i ) {
+    for ( int i = 0; i < sz; ++i ) {
         BF_Block *block;
         BF_Block_Init(&block);
         CALL_BF(BF_AllocateBlock(blockFileID, block));
-        char * b_data = BF_Block_GetData(block);
-        memset(b_data, depth, sizeof(int));
+        char *b_data = BF_Block_GetData(block);
+        memcpy(b_data, &depth, sizeof(int));
+        //memcpy(b_data+ sizeof(int))
         CALL_BF(BF_Block_SetDirty(block));
-
-        memset(secondBlockData+(2*i)*sizeof(int) ,i, sizeof(int));
-        memset(secondBlockData+(2*i)*sizeof(int)+sizeof(int) ,i+3, sizeof(int));
+        int b = i + 3;
+        memcpy(secondBlockData + (2 * i) * sizeof(int), &i, sizeof(int));
+        memcpy(secondBlockData + (2 * i) * sizeof(int) + sizeof(int), &b, sizeof(int));
 
     }
 
@@ -70,58 +70,61 @@ HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
 
 HT_ErrorCode HT_OpenIndex(const char *fileName, int *indexDesc) {
     //insert code here
-    for ( int i = 0; i <MAX_OPEN_FILES ; ++i ) {
-        if(OpenHashFiles[i].FileName==NULL){
-            OpenHashFiles[i].FileName= malloc((strlen(fileName)+1)*sizeof(char));
-            strcpy(OpenHashFiles[i].FileName,fileName);
-            CALL_BF(BF_OpenFile(fileName,&OpenHashFiles[i].BFid));
-            *indexDesc=i;
+    for ( int i = 0; i < MAX_OPEN_FILES; ++i ) {
+        if ( OpenHashFiles[i].FileName == NULL ) {
+            OpenHashFiles[i].FileName = malloc((strlen(fileName) + 1) * sizeof(char));
+            strcpy(OpenHashFiles[i].FileName, fileName);
+            CALL_BF(BF_OpenFile(fileName, &OpenHashFiles[i].BFid));
+            *indexDesc = i;
             return HT_OK;
         }
     }
-    printf("NO SPACE AVAILABLE FOR HASHFILE :%s\n",fileName);
+    printf("NO SPACE AVAILABLE FOR HASHFILE :%s\n", fileName);
     return HT_ERROR;
 }
 
 HT_ErrorCode HT_CloseFile(int indexDesc) {
     //insert code here
-    if(indexDesc<0 || indexDesc>MAX_OPEN_FILES){
+    if ( indexDesc < 0 || indexDesc > MAX_OPEN_FILES ) {
         printf("INDEX OUT OF TABLE LENGTH\n");
         return HT_ERROR;
     }
-    if(OpenHashFiles[indexDesc].FileName==NULL)printf("THERE IS NO OPEN FILE AT THIS INDEX\n");
+    if ( OpenHashFiles[indexDesc].FileName == NULL )printf("THERE IS NO OPEN FILE AT THIS INDEX\n");
     CALL_BF(BF_CloseFile(OpenHashFiles[indexDesc].BFid));
     free(OpenHashFiles[indexDesc].FileName);
-    OpenHashFiles[indexDesc].FileName=NULL;
+    OpenHashFiles[indexDesc].FileName = NULL;
     return HT_OK;
 }
 
 HT_ErrorCode HT_InsertEntry(int indexDesc, Record record) {
     //insert code here
     BF_Block *firstBlock, *secondBlock;
-    char *firstBlockData, * secondBlockData;
+    char *firstBlockData, *secondBlockData;
     char HtAddress[20];
+    int fileDesc = OpenHashFiles[indexDesc].BFid;
 
-    CALL_BF(BF_GetBlock(OpenHashFiles[indexDesc].BFid,1,firstBlock));
-    firstBlockData= BF_Block_GetData(secondBlock);
+    CALL_BF(BF_GetBlock(fileDesc, 1, firstBlock));
+    firstBlockData = BF_Block_GetData(secondBlock);
 
     char gd[10];
     memmove(gd, firstBlockData + strlen("HF"), sizeof(int));
     int global_depth = atoi(gd);
 
     /* we retrieve the address of the hashtable from the second block*/
-    CALL_BF(BF_GetBlock(OpenHashFiles[indexDesc].BFid,2,secondBlock));
-    secondBlockData= BF_Block_GetData(secondBlock);
+    CALL_BF(BF_GetBlock(fileDesc, 2, secondBlock));
+    secondBlockData = BF_Block_GetData(secondBlock);
 
     //hash function
-    int rec_id=record.id, block_index=0;
-    for(int i=0; i<global_depth; i++) {
-        block_index += (rec_id%2)*(int)pow(2, depth-i-1);
-        rec_id/=2;
+    int rec_id = record.id, block_index = 0;
+    for ( int i = 0; i < global_depth; i++ ) {
+        block_index += (rec_id % 2) * (int) pow(2, depth - i - 1);
+        rec_id /= 2;
     }
-
-    memset
-
+    int destinationBlock;
+    memcpy(secondBlockData + (2 * block_index + 1) * sizeof(int), &destinationBlock, sizeof(int));
+    BF_Block *blockToWrite;
+    CALL_BF(BF_GetBlock(fileDesc, destinationBlock, blockToWrite));
+    /*insertion of data*/
 
     return HT_OK;
 }
